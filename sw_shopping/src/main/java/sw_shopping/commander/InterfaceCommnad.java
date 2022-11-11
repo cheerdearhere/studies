09ea361f0db469baeb816964d4750a1e4142a487
DreamIk <dream-ik89@naver.com>
2022-11-11 23:10:03 +0900

    ssss

--- sw_shopping/src/main/java/sw_shopping/commander/InterfaceCommnad.java
+++
@@ -1,8 +0,0 @@
-package sw_shopping.commander;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface InterfaceCommnad {
-	public void excute(HttpServletRequest request, HttpServletResponse response); 
-}
