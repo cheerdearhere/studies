41f7d8839b70442ec1762075217c664c095ae56f
DreamIk <dream-ik89@naver.com>
2022-11-14 16:42:01 +0900

    q1: rename, q2: new

--- src/main/java/kwangClass/Question02.java
+++ src/main/java/kwangClass/Question02.java
@@ -16,6 +16,7 @@ public class Question02 {
 		int distance = scanner.nextInt();
 		System.out.println("세 친구("+human1+", "+human2+", "+human3+")가 있다. \n 셋의 집은 각각 10km마다 떨어져 있는 거리에 존재한다. \n 세 명의 집을 차례대로 방문하려면 몇 km를 걸어가야하는가? \n 답: ");
 		int result = scanner.nextInt();
+		scanner.close();
 		int answer = distance*3;
 		if(result==answer) {
 			System.out.println("정답입니다. ");
