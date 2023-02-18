4a1333b050c1293e2fafbf1861a3c9e7c6e0483b
dreamik <dream-ik89@naver.com>
2023-02-19 00:02:15 +0900

    Feat: 랜덤뽑기
    뽑기 품목은 최대 10개까지 등록 가능, user는 딱히 제한 없음.
    userData로 손님별 호출은 미구현(arrayList에 정장은 함.)

--- src/main/java/kwangClass/RandomPicker.java
+++ src/main/java/kwangClass/RandomPicker.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RandomPicker {
-
+	public static boolean isNumeric(String s) {
+		return s.chars().allMatch(Character::isDigit);
+	}
 	public static void main(String[] args) {
 		boolean storeLoof=true;
 		boolean userLoof=true;
 		boolean pickLoof=true;
 		String responsYN;
 		int income = 0;
+		int rateSum=0;
 		
-		int itemNum=1;
+		int itemNum=0;
 		String itemName;
 		int itemValue;
 		int itemRate;
@@ -21,34 +24,53 @@ public class RandomPicker {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("판매 물품을 정합니다.");
+		System.out.println("뽑기 물품을 입력합니다(최대 10개).");
 		while(storeLoof) {
-			System.out.println("no: "+itemNum);
+			System.out.println("no: "+(itemNum));
 			System.out.println("물품명: ");
-			itemName=scanner.nextLine();
+			responsYN=scanner.next();
+			itemName=responsYN;
 			System.out.println("물품 원가: ");
-			itemValue=scanner.nextInt();
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("숫자를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			itemValue=Integer.parseInt(responsYN);
 			System.out.println("확률(양수): ");
-			itemRate=scanner.nextInt();
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("자연수를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			itemRate=Integer.parseInt(responsYN);
 			System.out.println("물품 설명: ");
-			itemInfo=scanner.nextLine();
+			itemInfo=scanner.next();
 			
 			PickStore item = new PickStore(itemNum,itemName,itemValue,itemRate,itemInfo);
-			itemList.add(itemNum,item);
-			System.out.println(item.getItemNum()+"번("+item.getItemName()+") 등록완료\n"
-					+ "더 등록하시겠습니까?(y/n)");
-			responsYN= scanner.nextLine();
+			itemList.add(item);
+			System.out.println(item.getItemNum()+"번("+item.getItemName()+") 등록완료");
+			if(itemNum==9) {//0~9: 10개
+				System.out.println("뽑기 물품 등록을 종료합니다. "+itemList.size()+"개 등록");
+				for(PickStore itemdata:itemList) {
+					rateSum+=itemdata.getItemRate();
+				}
+				for(PickStore itemdata:itemList) {
+					System.out.println(itemdata.getItemName()+"/rate: "+itemdata.getItemRate()+"/"+rateSum);
+				}
+				storeLoof=false;
+			}
+			System.out.println("더 등록하시겠습니까?(y/n)");
+			responsYN= scanner.next();
 			if(responsYN.equals("Y")||responsYN.equals("y")) {
 				itemNum+=1;
 				System.out.println("추가 정보를 입력하세요.");
 			}else if(responsYN.equals("N")||responsYN.equals("n")) {
 				System.out.println("뽑기 물품 등록을 종료합니다. "+itemList.size()+"개 등록");
-				int rateSum=0;
 				for(PickStore itemdata:itemList) {
 					rateSum+=itemdata.getItemRate();
 				}
 				for(PickStore itemdata:itemList) {
-					itemdata.setItemRate((int)(itemdata.getItemRate()/rateSum));
 					System.out.println(itemdata.getItemName()+"/rate: "+itemdata.getItemRate());
 				}
 				storeLoof=false;
@@ -70,20 +92,27 @@ public class RandomPicker {
 		System.out.println("뽑기권 구매");
 		while(userLoof) {
 			System.out.println((userNum+1)+"번째 손님이 오셨습니다.\n guest name: ");
-			userName=scanner.nextLine();
+			userName=scanner.next();
 			if(userName.trim().isEmpty()) {
 				System.out.println("데이터가 없습니다.");
-				userName=scanner.nextLine();
+				userName=scanner.next();
 			}
 			System.out.println("이곳에서 사용할 금액을 입력하세요.");
-			userAccount=scanner.nextInt();
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("숫자를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			userAccount=Integer.parseInt(responsYN);
 			System.out.println("계좌에 등록된 금액으로 뽑기권을 몇개 구매하시겠습니까? \n(개당 1,000원/ 10개 구입시 1개 추가/ 100개 구입시 1개 추가/ 1000개 구입시 200개 추가");
-			userTicket=scanner.nextInt();
-			if(userTicket<0||(""+userTicket).contains(".")) {
-				System.out.println("0보다 큰 자연수를 입력하셔야 합니다.");
-				userTicket=scanner.nextInt();
-			}else if(userAccount<(userTicket*1000)) {
-				System.out.println("잔액이 부족합니다.");
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("숫자를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			userTicket=Integer.parseInt(responsYN);
+			if(userAccount<(userTicket*1000)) {
+				System.out.println("잔액이 부족합니다. 다시 진행해주세요");
 				continue;
 			}
 			
@@ -110,7 +139,7 @@ public class RandomPicker {
 			userList.add(user);
 			System.out.println(cost+"원을 사용해 뽑기권 "+userTicket+"개를 구매했습니다.\n잔액: "+userAccount);
 			System.out.println("더 구매하실 분이 있습니까?(y/n)");
-			responsYN=scanner.nextLine();
+			responsYN=scanner.next();
 			if(responsYN.equals("Y")||responsYN.equals("y")) {
 				userNum+=1;
 			}else if(responsYN.equals("N")||responsYN.equals("n")) {
@@ -120,28 +149,130 @@ public class RandomPicker {
 				System.out.println("잘못입력하셨습니다.  "+userList.size()+"명 구매함");
 			}
 		}
+		
 		//random picking 
 		ArrayList<PickStore> pickedList = new ArrayList<PickStore>();
-		int execiteNum=0;
+		double randomBox;
+		int runTimes;
+		int range1=itemList.get(0).getItemRate();
+		int range2=0, range3=0, range4=0, range5=0, range6=0, range7=0, range8=0, range9=0, range10=0;
+		if(itemList.size()>=2) {
+			range2=range1+itemList.get(1).getItemRate();
+		}
+		if(itemList.size()>=3) {
+			range3=range2+itemList.get(2).getItemRate();
+		}
+		if(itemList.size()>=4) {
+			range4=range3+itemList.get(3).getItemRate();
+		}
+		if(itemList.size()>=5) {
+			range5=range4+itemList.get(4).getItemRate();
+		}
+		if(itemList.size()>=6) {
+			range6=range5+itemList.get(5).getItemRate();
+		}
+		if(itemList.size()>=7) {
+			range7=range6+itemList.get(6).getItemRate();
+		}
+		if(itemList.size()>=8) {
+			range8=range7+itemList.get(7).getItemRate();
+		}
+		if(itemList.size()>=9) {
+			range9=range8+itemList.get(8).getItemRate();
+		}
+		if(itemList.size()>=10) {
+			range10=range9+itemList.get(9).getItemRate();
+		}
+		String resultSet="당첨결과: ";
+		System.out.println(rateSum+"//"+range1+"/"+range2+"/"+range3+"/"+range4+"/"+range5+"/"+range6+"/"+range7+"/"+range8+"/"+range9+"/"+range10);
 		while(pickLoof) {
 			System.out.println("현재 뽑기권 소유 현황");
 			for(UserData user:userList) {
-				System.out.println((user.getUserNum()+1)+". 이름: "+user.getUserName()+"/"+user.getUserTicket()+"개 \n");
+				System.out.println((user.getUserNum()+1)+". 이름: "+user.getUserName()+"/"+user.getUserTicket()+"개");
 			}
 			System.out.println("뽑기를 진행하실 회원님의 번호를 입력해주세요.");
-			execiteNum=scanner.nextInt();
-			if(execiteNum<1||(""+execiteNum).contains(".")) {
-				System.out.println("1보다 큰 자연수를 입력하셔야 합니다.");
-				userTicket=scanner.nextInt();
-			}else if(execiteNum>userList.size()) {
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("숫자를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			userNum=Integer.parseInt(responsYN);
+			if(userNum>userList.size()) {
 				System.out.println("회원수보다 많은 수를 입력하셨습니다. 다시 입력해주세요.");
-				userTicket=scanner.nextInt();
+				continue;
 			}
 			
-			
-			
+			UserData user=userList.get(userNum-1);
+			userTicket=user.userTicket;
+			System.out.println("몇번 수행하시겠습니까?(뽑기권:"+userTicket+"개)");
+			responsYN=scanner.next();
+			if(!isNumeric(responsYN)) {
+				System.out.println("숫자를 입력해주세요");
+				responsYN=scanner.next();
+			}
+			runTimes=Integer.parseInt(responsYN);
+			if(runTimes>userTicket) {
+				System.out.println("가지고 있는 뽑기권보다 많은 수를 입력하셨습니다. 처음으로 이동합니다.");
+				continue;
+			}
+			System.out.println(userNum+"번 회원님의 뽑기를 "+runTimes+"번 수행합니다.");
+			int j=0;
+			for(int i=0;i<runTimes;i++) {
+				j=i+1;
+				randomBox=(int)(Math.random()*(rateSum-1+1)+1);
+				resultSet+="@"+j+"결과("+randomBox+")=>";
+				if(1<=randomBox&&randomBox<=range1) {
+					PickStore pickedItem=itemList.get(0);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range2&&range2!=0) {
+					PickStore pickedItem=itemList.get(1);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range3&&range3!=0){
+					PickStore pickedItem=itemList.get(2);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range4&&range4!=0){
+					PickStore pickedItem=itemList.get(3);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range5&&range5!=0){
+					PickStore pickedItem=itemList.get(4);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range6&&range6!=0){
+					PickStore pickedItem=itemList.get(5);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range7&&range7!=0){
+					PickStore pickedItem=itemList.get(6);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range8&&range8!=0){
+					PickStore pickedItem=itemList.get(7);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range9&&range9!=0){
+					PickStore pickedItem=itemList.get(8);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}else if(randomBox<=range10&&range10!=0){
+					PickStore pickedItem=itemList.get(9);
+					pickedList.add(pickedItem);
+					resultSet+=pickedItem.getItemName()+"//";
+				}
+				if((j%5)==0) {
+					resultSet+="\n";
+				}
+			}
+			System.out.println(resultSet);
+			user.setPickedItem(pickedList);
+			userTicket-=runTimes;
+			user.setUserTicket(userTicket);
+			System.out.println("남은 뽑기권: "+userTicket);
 			System.out.println("더 뽑으실 분 있습니까?(y/n)");
-			responsYN=scanner.nextLine();
+			responsYN=scanner.next();
 			if(responsYN.equals("Y")||responsYN.equals("y")) {
 				System.out.println(userList.size()+"명 진행 중");
 			}else if(responsYN.equals("N")||responsYN.equals("n")) {
@@ -151,9 +282,8 @@ public class RandomPicker {
 				System.out.println("잘못입력하셨습니다.  "+userList.size()+"명 진행.");
 			}
 		}
-		
 		scanner.close();
-		System.out.println("영업종료\n오늘매출: "+income);
+		System.out.println("#######영업종료#######\n오늘매출: "+income+"원");
 	}
 
 }
@@ -211,7 +341,7 @@ class PickStore{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public PickStore(int itemNum, String itemName, int itemRate, int itemValue, String itemInfo) {
+	public PickStore(int itemNum, String itemName, int itemValue, int itemRate, String itemInfo) {
 		super();
 		this.itemName = itemName;
 		this.itemNum = itemNum;
