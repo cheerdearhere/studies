b59112d7ed1b60be4b29aa8f2b0b01dfb60d335b
dreamik <dream-ik89@naver.com>
2023-03-06 20:55:23 +0900

    Study:NumberTower의 client부 구현
    반복문의 분기구별

--- src/main/java/kwangClass/NumberTower.java
+++ src/main/java/kwangClass/NumberTower.java
@@ -1,34 +1,88 @@
 package kwangClass;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NumberTower {
-
+	static FloorCalc floorCalc;
 	public static void main(String[] args) {
-		FloorCalc test1 = new FloorCalc(1);
-		test1.getFloorL();
-		test1.getFloorR();
-		test1.allFloor();
+		Scanner inputScanner = new Scanner(System.in);
+		String clientResponse = "";
+		int select=0;
+		int floor=0;
 		
-		FloorCalc test2 = new FloorCalc(2);
-		test2.getFloorL();
-		test2.getFloorR();
-		test2.allFloor();
-
-		FloorCalc test3 = new FloorCalc(3);
-		test3.getFloorL();
-		test3.getFloorR();
-		test3.allFloor();
-		
-		FloorCalc test4 = new FloorCalc(4);
-		test4.getFloorL();
-		test4.getFloorR();
-		test4.allFloor();
-		
-		FloorCalc test20 = new FloorCalc(20);
-		test20.getFloorL();
-		test20.getFloorR();
-		test20.allFloor();
+		floorLoof:
+		while(true) {
+			System.out.println("System: 몇 층으로 쌓으시겠습니까?");
+			clientResponse=inputScanner.next();
+			if(clientResponse.trim()=="") {
+				System.out.println("공백은 입력할 수 없습니다.");
+				continue floorLoof;
+			}
+			try {
+				floor=Integer.parseInt(clientResponse);
+			}
+			catch(NumberFormatException e) {
+				System.out.println("문자는 입력할 수 없습니다.");
+				continue floorLoof;
+			}
+			if(floor==0) {
+				floorCalc = new FloorCalc(floor);
+				continue floorLoof;
+			}
+			floorCalc = new FloorCalc(floor);
+			calcLoof:
+				while(true) {
+					System.out.println("""
+							기능을 선택해주세요
+								1. 마지막층의 왼쪽
+								2. 마지막층의 오른쪽
+								3. 모든 층 양끝의 수
+								4. 층 다시 정하기
+								5. 전체 종료
+							""");
+					clientResponse = inputScanner.next();
+					if(clientResponse.trim()=="") {
+						System.out.println("공백은 입력할 수 없습니다.");
+						continue calcLoof;
+					}
+					try {
+						select=Integer.parseInt(clientResponse);
+					}
+					catch(NumberFormatException e) {
+						System.out.println("문자는 입력할 수 없습니다.");
+						continue calcLoof;
+					}
+					if(select==1) {
+						System.out.println("숫자탑의 제일 아래 왼쪽");
+						floorCalc.getFloorL();
+						continue calcLoof;
+					}
+					else if(select==2) {
+						System.out.println("숫자탑의 제일 아래 오른쪽");
+						floorCalc.getFloorR();
+						continue calcLoof;
+					}
+					else if(select==3) {
+						System.out.println("숫자탑의 모든 층 양끝");
+						floorCalc.allFloor();
+						continue calcLoof;
+					}
+					else if(select==4) {
+						System.out.println("숫자탑을 다시 설정합니다. \n 현재: "+floor);
+						continue floorLoof;
+					}
+					else if(select==5) {
+						System.out.println("시스템을 종료합니다.");
+						inputScanner.close();
+						break floorLoof;
+					}
+					else {
+						System.out.println("잘못입력하셨습니다: "+select);
+						continue calcLoof;
+					}
+				}
+		}
 	}
 }
 
@@ -36,13 +90,17 @@ class FloorCalc{
 	private int floor;
 	private int floorL;
 	private int floorR;
-	private ArrayList<int[]> floorList = new ArrayList<int[]>();
+	private ArrayList<int[]> floorList;
 	private int[] floorData = new int[3];//0:floor, 1:floorL, 2:floorR
 	
 	public FloorCalc(int n) {
 		this.floor=n;
-		if(n<=0)System.out.println("0 이하의 정수는 입력할 수 없습니다.");
+		if(n<=0) {
+			System.out.println("0 이하의 정수는 입력할 수 없습니다.");
+			return;
+		}
 		else {
+			floorList= new ArrayList<int[]>();
 			int d=2;
 			floorL=floorR=1;
 			floorData=new int[] {0,1,1};
@@ -57,6 +115,7 @@ class FloorCalc{
 			}
 		}
 	}
+	
 	public void getFloorL() {
 		System.out.println(floor+"층 L: "+floorL);
 	}
